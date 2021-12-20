045adc1b8dd092fc08d142036cfb2ef8595a86d4
arunkarthick <arunkarthicknm@gmail.com>
2021-12-20 14:01:52 +0530

    Fetched all rows and built banner

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
+import Banner from './Banner'
 
 function App() {
   return (
     <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      {/* nav */}
+      <Banner/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
       <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending}/>
-     
+      <Row title="TOP RATED" fetchUrl={requests.fetchTopRated}/>
+      <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="COMEDY MOVIES" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="ROMANCE MOVIES" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries}/>
+
+
+
     </div>
   );
 }
