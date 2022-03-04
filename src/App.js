478c142e1488621ae021a75ac124ef601ccad980
arunkarthick <arunkarthicknm@gmail.com>
2022-03-04 20:50:24 +0530

    Successfully deployed initial version version 1.0

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Row from './Row';
 import requests from './requests';
 import Banner from './Banner'
+import Nav from './Nav';
+
 
 function App() {
   return (
-    <div className="App">
-      {/* nav */}
+    <div  className="App">
+      <Nav />
       <Banner/>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
       <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending}/>
       <Row title="TOP RATED" fetchUrl={requests.fetchTopRated}/>
       <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies}/>
@@ -17,7 +19,7 @@ function App() {
       <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies}/>
       <Row title="ROMANCE MOVIES" fetchUrl={requests.fetchRomanceMovies}/>
       <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries}/>
-
+    
 
 
     </div>
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className= "TITLE"> 
+        <h2> Designed and Developed by arunkarthicknm</h2>
+        </div>
+    )
+}
+
+export default Footer
